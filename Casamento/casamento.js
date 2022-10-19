80fd8cc98066c894a133823cf7fc9abeb9679d82
LucasLucky1 <lucaserickson100@gmail.com>
2022-10-19 02:25:52 -0300

    trabalhando na section padrinhos, adicionando onscroll js e modificando referencias

--- Casamento/casamento.js
+++ Casamento/casamento.js
@@ -1,75 +1,4 @@
 
-
-// const BlocoPadrinhos1= document.querySelector(".bloco-padrinhos-1")
-// const BlocoPadrinhos2= document.querySelector(".bloco-padrinhos-2")
-// const BlocoPadrinhos3= document.querySelector(".bloco-padrinhos-3")
-// const BlocoPadrinhos4= document.querySelector(".bloco-padrinhos-4")
-
-// const quadrado1= document.querySelector('.quadrado-1');
-// const quadrado2= document.querySelector('.quadrado-2');
-// const quadrado3= document.querySelector('.quadrado-3');
-// const quadrado4= document.querySelector('.quadrado-4');
-
-// const botaoPlay= document.querySelector('.play-normal');
-
-
-
-
-
-
-
-// quadrado1.onclick= function(){
-//     if (BlocoPadrinhos1.classList[1] === 'oculto'){  
-//         BlocoPadrinhos1.classList.remove('oculto');
-
-        
-//     }else{
-//         BlocoPadrinhos1.classList.add('oculto');
-        
-//      }
-// }
-
-
-
-// quadrado2.onclick= function(){
-//     if (BlocoPadrinhos2.classList[1] === 'oculto'){  
-//         BlocoPadrinhos2.classList.remove('oculto');
-
-        
-//     }else{
-//         BlocoPadrinhos2.classList.add('oculto');
-        
-//      }
-// }
-
-
-// quadrado3.onclick= function(){
-//     if (BlocoPadrinhos3.classList[1] === 'oculto'){  
-//         BlocoPadrinhos3.classList.remove('oculto');
-
-        
-//     }else{
-//         BlocoPadrinhos3.classList.add('oculto');
-        
-//      }
-// }
-
-
-// quadrado4.onclick= function(){
-//     if (BlocoPadrinhos4.classList[1] === 'oculto'){  
-//         BlocoPadrinhos4.classList.remove('oculto');
-
-        
-//     }else{
-//         BlocoPadrinhos4.classList.add('oculto');
-        
-//      }
-// }
-
-
-
-
-
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
     navigation: {
@@ -82,3 +11,12 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+
+  
+
+  
+  window.addEventListener('scroll', function(){
+    var cabecalho = document.querySelector('.cabecalho');
+    cabecalho.classList.toggle("teste", window.scrollY > 100);
+
+  })
