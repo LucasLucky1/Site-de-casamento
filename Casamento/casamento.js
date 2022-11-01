e0af317fe54822a8660fa14643278040a2d0eedf
LucasLucky1 <lucaserickson100@gmail.com>
2022-11-01 15:27:01 -0300

    commit para vercel

--- Casamento/casamento.js
+++ Casamento/casamento.js
@@ -19,20 +19,18 @@ var swiper = new Swiper(".mySwiper", {
     var cabecalho = document.querySelector('.cabecalho');
     cabecalho.classList.toggle("cabecalho-move", window.scrollY > 100);
     var cabecalhoItens = document.querySelectorAll('.cabecalho-item');
-    cabecalhoItens[0].classList.toggle("cabecalho-item-move", window.scrollY > 100);
-    cabecalhoItens[1].classList.toggle("cabecalho-item-move", window.scrollY > 100);
-    cabecalhoItens[2].classList.toggle("cabecalho-item-move", window.scrollY > 100);
-    cabecalhoItens[3].classList.toggle("cabecalho-item-move", window.scrollY > 100);
-    cabecalhoItens[4].classList.toggle("cabecalho-item-move", window.scrollY > 100);
-    
 
+    for (var x = 0; x < cabecalhoItens.length; x++) {
+      cabecalhoItens[x].classList.toggle("cabecalho-item-move", window.scrollY > 100);
+      
+    }
   })
 
   window.addEventListener('scroll', function(){
     var cabecalhoItens = document.querySelectorAll('.cabecalho-item');
-    cabecalhoItens[0].classList.toggle("ativo", window.scrollY >= 0  & window.scrollY < 700);
-    cabecalhoItens[1].classList.toggle("ativo", window.scrollY >= 700 & window.scrollY < 1450);
-    cabecalhoItens[2].classList.toggle("ativo", window.scrollY >= 1450 & window.scrollY < 3230);
-    cabecalhoItens[3].classList.toggle("ativo", window.scrollY >= 3230 & window.scrollY < 3900);
-    cabecalhoItens[4].classList.toggle("ativo", window.scrollY >= 3900 & window.scrollY < 5000);
+    cabecalhoItens[0].classList.toggle("cabecalho-ativo", window.scrollY >= 0  & window.scrollY < 700);
+    cabecalhoItens[1].classList.toggle("cabecalho-ativo", window.scrollY >= 700 & window.scrollY < 1450);
+    cabecalhoItens[2].classList.toggle("cabecalho-ativo", window.scrollY >= 1450 & window.scrollY < 3230);
+    cabecalhoItens[3].classList.toggle("cabecalho-ativo", window.scrollY >= 3230 & window.scrollY < 3900);
+    cabecalhoItens[4].classList.toggle("cabecalho-ativo", window.scrollY >= 3900 & window.scrollY < 5000);
   })
