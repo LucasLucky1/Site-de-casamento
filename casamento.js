22f336cc11a7f38f9fc0c244e10b8ee83ec1c846
LucasLucky1 <lucaserickson100@gmail.com>
2022-11-06 18:00:18 -0300

    Adicionando section 'presentes' e modificando botão 'exibir'

--- casamento.js
+++ casamento.js
@@ -10,12 +10,12 @@ var swiper = new Swiper(".mySwiper", {
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
 
   
 
   
-  window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function(){
     var cabecalho = document.querySelector('.cabecalho');
     cabecalho.classList.toggle("cabecalho-move", window.scrollY > 100);
     var cabecalhoItens = document.querySelectorAll('.cabecalho-item');
@@ -24,13 +24,26 @@ var swiper = new Swiper(".mySwiper", {
       cabecalhoItens[x].classList.toggle("cabecalho-item-move", window.scrollY > 100);
       
     }
-  })
+});
 
-  window.addEventListener('scroll', function(){
+  
+
+
+
+window.addEventListener('scroll', function(){
     var cabecalhoItens = document.querySelectorAll('.cabecalho-item');
     cabecalhoItens[0].classList.toggle("cabecalho-ativo", window.scrollY >= 0  & window.scrollY < 700);
     cabecalhoItens[1].classList.toggle("cabecalho-ativo", window.scrollY >= 700 & window.scrollY < 1450);
     cabecalhoItens[2].classList.toggle("cabecalho-ativo", window.scrollY >= 1450 & window.scrollY < 3230);
     cabecalhoItens[3].classList.toggle("cabecalho-ativo", window.scrollY >= 3230 & window.scrollY < 3900);
     cabecalhoItens[4].classList.toggle("cabecalho-ativo", window.scrollY >= 3900 & window.scrollY < 5000);
-  })
+});
+
+
+
+
+var exibir = document.querySelector(".botao-exibir");
+exibir.addEventListener("click", function(){
+    var presentes = document.querySelector(".presentes")
+    presentes.classList.toggle("exibir-mais")
+});
